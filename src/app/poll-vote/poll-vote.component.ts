7613c84e94afd92919c386b8ecfaae0fd3ab62fe
Greg Delgado <grd888@gmail.com>
2021-05-23 13:51:55 +0800

    display poll results in charts

--- src/app/poll-vote/poll-vote.component.ts
+++ src/app/poll-vote/poll-vote.component.ts
@@ -1,13 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
+import ApexCharts from 'apexcharts';
 @Component({
   selector: 'app-poll-vote',
   templateUrl: './poll-vote.component.html',
   styleUrls: ['./poll-vote.component.scss'],
 })
 export class PollVoteComponent implements OnInit {
-  options = ['Monday', 'Tuesday', 'Wednesday'];
+  @Input() voted: boolean = false;
+  @Input() options: string[] = [];
+  @Input() results: number[] = [];
+  @Input() question: string = '';
+  // options = ['Monday', 'Tuesday', 'Wednesday'];
   voteForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
@@ -16,9 +20,42 @@ export class PollVoteComponent implements OnInit {
     });
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (this.voted) {
+      this.generateChart();
+    }
+  }
 
   submitForm() {
     console.log(this.voteForm.value);
+  }
+
+  generateChart() {
+    const options: ApexCharts.ApexOptions = {
+      series: [
+        {
+          data: this.results,
+        },
+      ],
+      chart: {
+        height: 350,
+        type: 'bar',
+      },
+      plotOptions: {
+        bar: {
+          columnWidth: '45%',
+          distributed: true,
+        },
+      },
+      legend: {
+        show: false,
+      },
+      xaxis: {
+        categories: this.options,
+      },
+    };
+
+    const chart = new ApexCharts(document.getElementById('poll-results'), options);
+    chart.render();
   }
 }
